eb749eb13005880ec6d28321e9da8c68d55c9d34
erikiado <erikiado@gmail.com>
2020-06-14 13:04:38 -0500

    Add get item from slot list to utils

--- lambda/py/alexa/util.py
+++ lambda/py/alexa/util.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 import random
+import six
+# from ask_sdk_core.handler_input import HandlerInput
+# from ask_sdk_core.utils import is_request_type
 
 from . import data
 from typing import Dict
@@ -20,3 +23,20 @@ def get_random_item(locale):
     """Return a random item from the locale specific dict."""
     # type: (str) -> str
     return random.choice(list(load_locale_specific_recipe(locale).keys()))
+
+
+def get_item(slots, states_list):
+    """Get matching data object from slot value."""
+    item = []
+    resolved_slot = None
+    for _, slot in six.iteritems(slots):
+        if slot.value is not None:
+            resolved_slot = slot.value
+            for state in states_list:
+                for _, v in six.iteritems(state):
+                    if v.lower() == slot.value.lower():
+                        item.append(state)
+            if len(item) > 0:
+                return item[0], True
+    else:
+        return resolved_slot, False
